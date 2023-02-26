d88e6d745c4e6a79b15810dcc55438714e2a86bc
Noah Wiggin <wigginno@oregonstate.edu>
2023-02-26 16:00:42 -0500

    Update dataset_urls.json and parallelize dataset retrieval

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 
 \#TODO: Finish README
 
-Clone this repo:
+### Clone this repo:
 ```
 git clone https://github.com/fogg-lab/engineering-high-throughput-screening-platforms-of-cervical-cancer.git
 ```
 
-Data retrieval script usage (download and extract images from the dataset):
+### Data retrieval script - download and extract images from the dataset
+**Prerequisites**:  
+- [Python 3.7 or higher](https://www.python.org/downloads/)
+- urllib3 (`python3 -m pip install -r requirements.txt` or `python3 -m pip install urllib3`)
+#### Usage:
 ```
 python3 get_images.py
 # then follow the prompts
--- get_images.py
+++ get_images.py
@@ -1,70 +1,111 @@
 import os
 import os.path as osp
 import json
-import urllib.request
+from multiprocessing import Pool as ThreadPool
+import urllib3.request
 import zipfile
 from pathlib import Path
 
-print("This script will download and extract the images from the dataset.")
-print(f"\nDefault output directory: {osp.join(os.getcwd(), 'data')}")
+from time import sleep
 
-output_dir = input(f"\nSpecify output directory or leave blank for default: ")
-if output_dir == "":
-    output_dir = osp.join(os.getcwd(), 'data')
+MAX_PARALLEL_EXTRACTIONS = 3        # Maximum number of archives to extract simultaneously
+DOWNLOAD_ATTEMPTS = 3               # Number of times to retry a download if it fails
+ABORT_ON_FAILED_DOWNLOAD = False    # Abort the script if a download fails
 
-# Make sure the parent directory of the output directory exists
-parent_dir = Path(output_dir).parent
-if not parent_dir.exists():
-    raise Exception(f"Directory {parent_dir} does not exist.")
+# Async function to extract files and delete archive
+def decompress_and_delete(filepath):        
+    with zipfile.ZipFile(filepath, "r") as zip_ref:
+        zip_ref.extractall(osp.dirname(filepath))
+    os.remove(filepath)
+    print(f"Extracted images from {osp.basename(filepath)} and deleted archive.")
 
-# Create the output directory if it doesn't exist
-os.makedirs(output_dir, exist_ok=True)
+def main():
+    print("This script will download and extract the images from the dataset.")
+    print(f"\nDefault output directory: {osp.join(os.getcwd(), 'data')}")
 
-# Load the JSON file
-with open("dataset_urls.json", "r") as f:
-    data = json.load(f)
+    output_dir = input(f"\nSpecify output directory or leave blank for default: ")
+    if output_dir == "":
+        output_dir = osp.join(os.getcwd(), 'data')
 
-# Ask which sets of images to download (or all of them)
-print("\nAvailable sets of images:")
-for i, set_name in enumerate(data["image_urls_by_set"].keys()):
-    print(f"{i+1}. {set_name}")
+    # Make sure the parent directory of the output directory exists
+    parent_dir = Path(output_dir).parent
+    if not parent_dir.exists():
+        raise Exception(f"Directory {parent_dir} does not exist.")
 
-print("\nNOTE: All image sets combined are ~1TB in size after extraction.")
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
 
-set_indices = input(
-    "Enter a comma-separated list of set numbers to download or leave blank to download all sets: ")
-if set_indices == "":
-    set_indices = list(range(1, len(data["image_urls_by_set"]) + 1))
-else:
-    set_indices = set_indices.replace(" ", "")
-    set_indices = [int(i) for i in set_indices.split(",")]
+    # Load the JSON file
+    with open("custom.json", "r") as f:
+        urls_by_set = json.load(f)
 
-print("Downloading and extracting the following image sets:")
-for i in set_indices:
-    print(f"{i}. {list(data['image_urls_by_set'].keys())[i-1]}")
+    # Ask which sets of images to download (or all of them)
+    print("\nAvailable sets of images:")
+    for i, set_name in enumerate(urls_by_set.keys()):
+        print(f"{i+1}. {set_name}")
 
-# Iterate over each set of images and download/extract them
-for i, (set_name, urls) in enumerate(data["image_urls_by_set"].items()):
-    if i+1 not in set_indices:
-        continue
+    print("\nNOTE: All image sets combined are ~1TB in size after extraction.")
 
-    # Create a subdirectory for this set of images
-    set_dir = os.path.join(output_dir, set_name)
-    os.makedirs(set_dir, exist_ok=True)
+    set_indices = input(
+        "Enter a comma-separated list of set numbers to download or leave blank to download all sets: ")
+    if set_indices == "":
+        set_indices = list(range(1, len(urls_by_set) + 1))
+    else:
+        set_indices = set_indices.replace(" ", "")
+        set_indices = [int(i) for i in set_indices.split(",")]
 
-    # Download and extract each ZIP file
-    for url in urls:
-        filename = osp.basename(url)
-        filepath = osp.join(output_dir, set_dir, filename)
-        print(f"Downloading {url} to {filepath}...")
+    print("Downloading and extracting the following image sets:")
+    for i in set_indices:
+        print(f"{i}. {list(urls_by_set.keys())[i-1]}")
 
-        urllib.request.urlretrieve(url, filepath)
+    # Iterate over each set of images and download/extract each ZIP file
+    for i, (set_name, urls) in enumerate(urls_by_set.items()):
+        if i+1 not in set_indices:
+            continue
 
-        print(f"Extracting {filepath}...")
-        with zipfile.ZipFile(filepath, "r") as zip_ref:
-            zip_ref.extractall(set_dir)
+        # Create a thread pool to decompress archives in parallel
+        # downloads will continue one at a time in the main thread
+        decompression_thread_pool = ThreadPool(MAX_PARALLEL_EXTRACTIONS)
 
-        # Delete the ZIP file after extraction
-        os.remove(filepath)
+        # Create a subdirectory for this set of images
+        set_dir = os.path.join(output_dir, set_name)
+        os.makedirs(set_dir, exist_ok=True)
+        http_request_pool = urllib3.PoolManager(num_pools=1)
+        for url in urls:
+            filename = osp.basename(url)
+            filepath = osp.join(output_dir, set_dir, filename)
+            print(f"Downloading {filename}...")
+            # download and save the file
+            for attempt in range(DOWNLOAD_ATTEMPTS):
+                try:
+                    r = http_request_pool.request('GET', url, preload_content=False)
+                    with open(filepath, 'wb') as f:
+                        data = r.read(1024)
+                        while data:
+                            f.write(data)
+                            data = r.read(1024)
+                    r.release_conn()
+                    break
+                except Exception as exc:
+                    print(f"Download failed for file {filename}: {exc}")
+                    if attempt == DOWNLOAD_ATTEMPTS - 1:
+                        action = "aborting" if ABORT_ON_FAILED_DOWNLOAD else "skipping"
+                        print(f"Download failed after {DOWNLOAD_ATTEMPTS} attempts. {action}.")
+                        if ABORT_ON_FAILED_DOWNLOAD:
+                            exit(1)
+                        continue
+                    print(f"Retrying download... ({attempt+1}/{DOWNLOAD_ATTEMPTS})")
+                    sleep(3)
 
-print("All images downloaded and extracted successfully.")
+            print(f"Extracting {filename}...")
+            decompression_thread_pool.apply_async(decompress_and_delete, (filepath,))
+
+        decompression_thread_pool.close()
+        decompression_thread_pool.join()
+
+        print(f"Done downloading and extracting {set_name}.")
+
+    print("All images downloaded and extracted successfully.")
+
+if __name__ == "__main__":
+    main()
